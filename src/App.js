a5efc68013bd33607eed12ad8fae83deaf272460
priyanka mohanty <117801319+SDEPriyanka@users.noreply.github.com>
2023-08-26 17:10:06 +0530

    client app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import ExploreCourses from "./ExploreCourses";
+function App() {
+  return (
+    <div className="App">
+      <ExploreCourses />
+    </div>
+  );
+}
+
+export default App;
